aa47cf1751782921cba05b31b4db298395701100
LuHeGi <boo122333@gmail.com>
2022-06-30 00:18:47 +0100

    Fix long break time

--- src/main/java/dev/luhegi/mods/simplecobblegen/CobbleGenBlock.java
+++ src/main/java/dev/luhegi/mods/simplecobblegen/CobbleGenBlock.java
@@ -6,14 +6,12 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 
 public class CobbleGenBlock extends BaseEntityBlock {
 
   public CobbleGenBlock() {
     super(Properties.copy(Blocks.COBBLESTONE)
-        .strength(15f)
-        .destroyTime(5f));
+        .strength(2f, 15f));
   }
 
   @Override
@@ -21,7 +19,6 @@ public class CobbleGenBlock extends BaseEntityBlock {
     return RenderShape.MODEL;
   }
 
-  @Nullable
   @Override
   public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
     return new CobbleGenEntity(p_153215_, p_153216_);
